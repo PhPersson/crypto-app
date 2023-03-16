5a17e136593cec572524969940269e749f8e3fa6
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-03-16 14:01:07 +0100

    Made a simple dropdown to change the number of days

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <bitcoin-chart />
+  </div>
+</template>
+
+<script>
+import BitcoinChart from './components/BitcoinChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BitcoinChart
+  }
+}
+</script>
+
+<style>
+/* .app {
+  max-width: 1200px;
+  margin: 0 auto;
+} */
+</style>
---
+++ src/components/BitcoinChart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bitcoin-chart">
+      <form>
+        <label>
+          Välj tidsperiod:
+          <select>
+            <option value="7">7 Dagar</option>
+            <option value="30">30 Dagar</option>
+            <option value="60">60 Dagar</option>
+          </select>
+        </label>
+      </form>
+
+    </div>
+</template>
+  
