16a80c530f3bc10a39df86b272b2eebf770f1f6e
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-06-03 13:36:51 +0200

    Made the articles center and adde a try catch block

--- src/components/CryptoNews.vue
+++ src/components/CryptoNews.vue
@@ -1,25 +1,39 @@
 <template>
-
 <div class="container">
-    
   <div class="row justify-content-center">
     <h1 class="display-3">News for the last 7 days from {{ this.getLast30Days() }}</h1>
-    <div class="col-md-6 col-lg-5 mb-4" v-for="article in articles" :key="article.title">
-      <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
-        <img :src="article.urlToImage" class="img-fluid" />
-        <a :href="article.url">
-          <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-        </a>
-      </div>
-      <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">News of the day</span>
-      <h4><strong>{{ article.title }}</strong></h4>
-      <p class="text-muted">{{ article.description }}</p>
-      <a :href="article.url" target="_blank" rel="noopener noreferrer">
-        <button type="button" class="btn btn-primary">Read more</button>
-      </a>
+
+    <div v-if="loading">
+
+      <p>Loading...</p>
     </div>
+
+    <div v-else-if="error">
+      <p>Error occurred while fetching the articles. Please try again later.</p>
+    </div>
+
+    <div v-else>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-5 mb-4" v-for="article in articles" :key="article.title">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img :src="article.urlToImage" class="img-fluid" />
+                <a :href="article.url">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">News of the day</span>
+            <h4><strong>{{ article.title }}</strong></h4>
+            <p class="text-muted">{{ article.description }}</p>
+            <a :href="article.url" target="_blank" rel="noopener noreferrer">
+                <button type="button" class="btn btn-primary">Read more</button>
+            </a>
+            </div>
+        </div>
+    </div>
+
   </div>
 </div>
+
 
 </template>
 
@@ -38,21 +52,20 @@ export default {
         this.fetchArticles();
     },
     methods: {
-        fetchArticles() {
+        async fetchArticles() {
         const apiKey =  `${process.env.VUE_APP_API_KEY}`
         const apiUrl = `https://newsapi.org/v2/everything?q=crypto&from=${this.getLast30Days()}&sortBy=publishedAt&apiKey=${apiKey}`;
 
-        axios
-            .get(apiUrl)
-            .then(response => {
+        try {
+            const response = await axios.get(apiUrl);
             this.articles = response.data.articles;
             this.loading = false;
-            })
-            .catch(error => {
+        } catch (error) {
             console.error(error);
             this.error = true;
             this.loading = false;
-            });
+        }
+
         },
 
         getLast30Days() {
@@ -61,7 +74,7 @@ export default {
             return pastDate.toLocaleDateString();
         }
     },
-    };
+};
 </script>
   
 <style scoped>
