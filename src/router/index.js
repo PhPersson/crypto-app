fd376d24c3dcf2b4136b9af14dd618ef8604d284
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-06-03 14:14:56 +0200

    Added a custom 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '@/views/HomePage.vue';
 import CryptoNews from '@/views/CryptoNews.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage},
   { path: '/news', name: 'News', component: CryptoNews},
+  { path: "/:catchAll(.*)", name: "NotFound", component: NotFound }
 ];
 
 const router = createRouter({
